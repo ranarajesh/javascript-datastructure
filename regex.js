edb0d8eb5c9840529af25597ec7e6b4b49b78d80
Rana Rajesh <rajesh86.rana@gmail.com>
2024-02-09 21:32:17 +0530

    update regex.js file

--- regex.js
+++ regex.js
@@ -8,6 +8,23 @@ charater`;
 
 // remove new line, spaces, special character from text and return length of text 
 console.log(`Text length `, sampleText.length);
-const regex = /\s/g;
+var regex = /\s/g;
 
 console.log(`Text length after apply regex ${sampleText.split(regex).join("").length}`);
+
+var str = "Hello, world 123!";
+
+var regex = /[a-zA-Z0-9]/g; // only count letters and numbers
+
+console.log(str.match(regex).length); // prints 13 to the console
+
+
+var str = "Hello, world 123!";
+
+var regex = /[^a-z]/gi; // only count letters
+
+console.log(str.replace(/[^a-z]/gi, "").length); // prints 10 to the consolex
+
+let count =0;
+Array.prototype.forEach.call( sampleText, i => ++count)
+console.log(count);
